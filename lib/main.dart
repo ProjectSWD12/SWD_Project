f45eaa6b74a86ea0711102d886c61259b7853ddb
ProjectSWD12 <projectswd12@gmail.com>
2025-06-18 20:34:51 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'screens/home_screen.dart';
 import 'screens/login_screen.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
   runApp(ProviderScope(child: MyApp()));
 }
 

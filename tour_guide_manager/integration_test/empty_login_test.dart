e1f2bf960b1a9731bb57688aef63d5136419bc95
Aleksey Chegaev <achegaev06@gmail.com>
2025-06-28 19:34:43 +0300

    last.

--- tour_guide_manager/integration_test/empty_login_test.dart
+++ tour_guide_manager/integration_test/empty_login_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:tour_guide_manager/main.dart' as app;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
